c5d3b20772230d32961b1a0d8e66249b94da6403
shonitta <shonitta.524@gmail.com>
2023-09-15 08:00:05 +0900

    [skip ci] update

--- ruby/basic-learning/func.rb
+++ ruby/basic-learning/func.rb
@@ -6,6 +6,11 @@ def sum_arr(arr)
 end
 
 
+def sum_hash_values(hash)
+  hash.values.sum
+end
+
+
 class TestSumArr < Minitest::Test
   def test_sum_arr_with_empty_array
     assert_equal 0, sum_arr([])
@@ -29,5 +34,32 @@ class TestSumArr < Minitest::Test
 
   def test_sum_arr_with_float_elements
     assert_equal 3.5, sum_arr([1.5, 2])
+  end
+end
+
+
+class TestSumHashValues < Minitest::Test
+  def tset_sum_hash_values_with_empty_hash
+    assert_equal 0, sum_hash_values({})
+  end
+
+  def test_sum_hash_values_with_one_element
+    assert_equal 1, sum_hash_values({ a: 1 })
+  end
+
+  def test_sum_hash_values_with_two_elements
+    assert_equal 3, sum_hash_values({ a: 1, b: 2 })
+  end
+
+  def test_sum_hash_values_with_three_elements
+    assert_equal 6, sum_hash_values({ a: 1, b: 2, c: 3 })
+  end
+
+  def test_sum_hash_values_with_negative_elements
+    assert_equal 0, sum_hash_values({ a: -1, b: 1 })
+  end
+
+  def test_sum_hash_values_with_float_elements
+    assert_equal 3.5, sum_hash_values({ a: 1.5, b: 2 })
   end
 end
